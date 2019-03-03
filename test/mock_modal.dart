a0271b1bcaf5e76f6bf50848a562e381f990821c
Louis <louis993546@gmail.com>
2019-03-03 17:14:30 +0100

    Add mock models

---
+++ test/mock_modal.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_create_submission/model.dart';
+import 'package:mockito/mockito.dart';
+
+class MockRecord extends Mock implements Record {}
+
+class MockDashboardConfig extends Mock implements DashboardConfig {}
