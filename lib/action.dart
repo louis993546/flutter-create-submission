85037cf65c6be4feaad42e0b7e051c6cb2d45854
Louis <louis993546@gmail.com>
2019-03-03 08:24:40 +0100

    Add a bunch of actions

--- lib/action.dart
+++ lib/action.dart
@@ -1,5 +1,48 @@
+import 'model.dart';
+
 class ClickAddRecordButton {}
+
+class ClickRecord {
+  Record record;
+
+  ClickRecord(this.record);
+}
+
+class ClickAddDashboardConfigButton {}
+
+class ClickEditDashboardConfigButton {
+  DashboardConfig dashboardConfig;
+
+  ClickEditDashboardConfigButton(this.dashboardConfig);
+}
+
+class ClickDeleteRecordButton {
+  Record record;
+
+  ClickDeleteRecordButton(this.record);
+}
+
+class ClickSaveRecordButton {
+  Record record;
+
+  ClickSaveRecordButton(this.record);
+}
 
 class ActionCreator {
   static ClickAddRecordButton clickAddRecordButton() => ClickAddRecordButton();
+
+  static ClickRecord clickRecord(Record record) => ClickRecord(record);
+
+  static ClickAddDashboardConfigButton clickAddDashboardConfigButton() =>
+      ClickAddDashboardConfigButton();
+
+  static ClickEditDashboardConfigButton clickEditDashboardConfigButton(
+          DashboardConfig dashboardConfig) =>
+      ClickEditDashboardConfigButton(dashboardConfig);
+
+  static ClickDeleteRecordButton clickDeleteRecordButton(Record record) =>
+      ClickDeleteRecordButton(record);
+
+  static ClickSaveRecordButton clicksaveRecordButton(Record record) =>
+      ClickSaveRecordButton(record);
 }
