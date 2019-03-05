067b524210abc1d3f7f8c3efef40897c0eda9327
Louis Tsai <louis993546@gmail.com>
2019-03-05 18:12:18 +0100

    Split ActionCreator out to it's own file

--- lib/action/action.dart
+++ lib/action/action.dart
@@ -1,3 +1,5 @@
+// TODO split this up by screen
+
 import '../model.dart';
 
 class ClickAddRecordButton {}
@@ -36,34 +38,3 @@ class ClickSaveRecordButton {
 }
 
 //TODO figure out how many "dismiss" action exists for add/edit record
-
-class ActionCreator {
-  static ClickAddRecordButton clickAddRecordButton() => ClickAddRecordButton();
-
-  static ClickRecord clickRecord(Record record) => ClickRecord(record);
-
-  static ClickAddDashboardConfigButton clickAddDashboardConfigButton() =>
-      ClickAddDashboardConfigButton();
-
-  static ClickDismissAddDashboardConfigButton
-      clickDismissAddDashboardConfigButton() =>
-          ClickDismissAddDashboardConfigButton();
-
-  static ClickEditDashboardConfigButton clickEditDashboardConfigButton(
-          DashboardConfig dashboardConfig) =>
-      ClickEditDashboardConfigButton(dashboardConfig);
-
-  static ClickDeleteRecordButton clickDeleteRecordButton() =>
-      ClickDeleteRecordButton();
-
-  static ClickCancelDeleteRecordButton clickCancelDeleteRecordButton() =>
-      ClickCancelDeleteRecordButton();
-
-  //TODO these 2 shouldn't exists: they should be handled by middleware
-  // static ClickConfirmDeleteRecordButton clickConfirmDeleteRecordButton(
-  //         Record record) =>
-  //     ClickConfirmDeleteRecordButton(record);
-
-  // static ClickSaveRecordButton clickSaveRecordButton(Record record) =>
-  //     ClickSaveRecordButton(record);
-}
---
+++ lib/action/creator.dart
@@ -0,0 +1,33 @@
+import '../model.dart';
+import 'action.dart';
+
+class ActionCreator {
+  static ClickAddRecordButton clickAddRecordButton() => ClickAddRecordButton();
+
+  static ClickRecord clickRecord(Record record) => ClickRecord(record);
+
+  static ClickAddDashboardConfigButton clickAddDashboardConfigButton() =>
+      ClickAddDashboardConfigButton();
+
+  static ClickDismissAddDashboardConfigButton
+      clickDismissAddDashboardConfigButton() =>
+          ClickDismissAddDashboardConfigButton();
+
+  static ClickEditDashboardConfigButton clickEditDashboardConfigButton(
+          DashboardConfig dashboardConfig) =>
+      ClickEditDashboardConfigButton(dashboardConfig);
+
+  static ClickDeleteRecordButton clickDeleteRecordButton() =>
+      ClickDeleteRecordButton();
+
+  static ClickCancelDeleteRecordButton clickCancelDeleteRecordButton() =>
+      ClickCancelDeleteRecordButton();
+
+  //TODO these 2 shouldn't exists: they should be handled by middleware
+  // static ClickConfirmDeleteRecordButton clickConfirmDeleteRecordButton(
+  //         Record record) =>
+  //     ClickConfirmDeleteRecordButton(record);
+
+  // static ClickSaveRecordButton clickSaveRecordButton(Record record) =>
+  //     ClickSaveRecordButton(record);
+}
